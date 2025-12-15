8667cdab1840e68eadba71f8edd79e2250ae7c95
Romain <contact@romain-neil.fr>
2025-12-15 22:29:52 +0100

    fix: update SongStreaming struct to change Ts type to string and adjust nullable fields

--- model/SongStreaming.go
+++ model/SongStreaming.go
@@ -1,7 +1,7 @@
 package model
 
 type SongStreaming struct {
-	Ts                            int    `json:"ts"`
+	Ts                            string `json:"ts"`
 	Username                      string `json:"username"`
 	Platform                      string `json:"platform"`
 	MsPlayed                      int    `json:"ms_played"`
@@ -10,16 +10,17 @@ type SongStreaming struct {
 	UserAgentDecrypted            string `json:"user_agent_decrypted"`
 	MasterMetadataTrackName       string `json:"master_metadata_track_name"`
 	MasterMetadataAlbumArtistName string `json:"master_metadata_album_artist_name"`
-	MasterMetadataAlbumName       string `json:"master_metadata_album_name"`
-	SpotifyTrackUri               string `json:"spotify_track_uri"`
-	EpisodeName                   string `json:"episode_name"`
-	EpisodeShowName               string `json:"episode_show_name"`
-	SpotifyEpisodeUri             string `json:"spotify_episode_uri"`
-	ReasonStart                   string `json:"reason_start"`
-	ReasonEnd                     string `json:"reason_end"`
-	Shuffle                       bool   `json:"shuffle"`
-	Skipped                       bool   `json:"skipped"`
-	Offline                       bool   `json:"offline"`
-	OfflineTimestamp              string `json:"offline_timestamp"`
-	IncognitoMode                 bool   `json:"incognito_mode"`
+	// Note: Spotify exports use "master_metadata_album_album_name" (double "album")
+	MasterMetadataAlbumName string  `json:"master_metadata_album_album_name"`
+	SpotifyTrackUri         string  `json:"spotify_track_uri"`
+	EpisodeName             *string `json:"episode_name"`
+	EpisodeShowName         *string `json:"episode_show_name"`
+	SpotifyEpisodeUri       *string `json:"spotify_episode_uri"`
+	ReasonStart             string  `json:"reason_start"`
+	ReasonEnd               string  `json:"reason_end"`
+	Shuffle                 bool    `json:"shuffle"`
+	Skipped                 *bool   `json:"skipped"`
+	Offline                 bool    `json:"offline"`
+	OfflineTimestamp        int64   `json:"offline_timestamp"`
+	IncognitoMode           bool    `json:"incognito_mode"`
 }
